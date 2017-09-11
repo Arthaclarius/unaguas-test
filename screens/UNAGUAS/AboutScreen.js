d6504433a29f5948820d3bdb18b2abf70120d9af
Arthaclarius <sgcg5@hotmail.com>
2017-09-11 06:05:42 -0600

    Problema con Textos inmensos

--- screens/UNAGUAS/AboutScreen.js
+++ screens/UNAGUAS/AboutScreen.js
@@ -4,11 +4,11 @@ import {
   Platform,
   ScrollView,
   StyleSheet,
-  Text,
   TouchableOpacity,
+  Text,
   View,
 } from 'react-native';
-import { Content, Button, Icon, Title, H1, H2, H3, Thumbnail } from 'native-base';
+import { Content, Button, Icon, Title, H1, H2, H3, Thumbnail, List, ListItem } from 'native-base';
 import Header from '../../components/HeaderUnaguas';
 import Footer from '../../components/FooterUnaguas';
 
@@ -24,13 +24,85 @@ export default class AboutScreen extends React.Component {
 
 
     const nPageContent = []
-    nPageContent.push({
-      type: 'Text',
-      val: `Creada el 14 de setiembre del 2002,
-       la Unión de Acueductos Griegos Unidos por
-        el Ambiente y la Salud, UNAGUAS, tiene dos
-         fines fundamentales:`
-    })
+
+    nPageContent.push(this._createContentPageItem('Text',
+    "Creada el 14 de setiembre del 2002, " +
+    "la Unión de Acueductos Griegos Unidos por " +
+    "Ambiente y la Salud, UNAGUAS, tiene dos " +
+    "fundamentales:"
+      , {flex: 1}))
+
+    nPageContent.push(this._createContentPageItem('List',
+    [`La conservación de los Recursos Naturales, Cuencas Hidrográficas y Zonas de Recarga Acuífera del Cantón de Grecia.`,
+    "Fortalecer el accionar de los Acueductos Comunales del cantón en procura de una adecuada gestión del recurso hídrico" +
+    "y de un servicio de agua potable en calidad y cantidad."]
+    , {flex: 1}))
+
+    nPageContent.push(this._createContentPageItem('Header',
+    'RESEÑA HISTÓRICA'
+    , {flex: 1}))
+
+    nPageContent.push(this._createContentPageItem('ScrollView',
+    "UNAGUAS se encuentra en el cantón de Grecia, a 45 kilómetros al oeste de San José, la capital del país, con una extensión de" +
+    " 395.72 km² y una población aproximada de 75 000 habitantes. Grecia está al pie de la Cordillera Volcánica Central, zona" +
+    " donde tienen origen varios mantos acuíferos y gran número de manantiales y ríos, de los cuales los Acueductos" +
+    " de la región y los agricultores obtienen agua para el consumo humano y riego. La preocupación por los cambios" +
+    " el uso del suelo en esa zona de alta precipitación y recarga acuífera motivó que personeros de varios Acueductos Comunales" +
+    " conversaciones para determinar cómo podían hacer frente a la situación. La expansión urbana y construcción de desarrollos" +
+    " en zonas altas con agradable clima y bellas vistas estaba provocando grandes presiones sobre el suelo y el servicio" +
+    " agua potable, estaba además amenazando con alterar zonas frágiles y vitales para el abastecimiento en agua potable para las" +
+    ". El consenso fue unir esfuerzos y realizar luchas en conjunto. Se pensó entonces en agrupar a los Acueductos Comunales" +
+    " una organización de segundo nivel, que velara por sus intereses y propiciara iniciativas conjuntas en procura de la protección" +
+    " los recursos naturales y el recurso hídrico de la zona y ese sería el fin con el que iba a ser creada la Unión en el año 2002." +
+    " embargo, pronto los Acueductos Comunales se dieron cuenta que, si ellos mismos no fortalecían sus capacidades, su accionar," +
+    " poco lo que podrían lograr. Eso propició el otro fin de UNAGUAS: Fortalecer el accionar de los Acueductos Comunales del cantón" +
+    " procura de una adecuada gestión del recurso hídrico y de un servicio de agua potable en cantidad, calidad y continuidad." +
+    " los años, UNAGUAS se ha convertido en una entidad de apoyo a los cometidos de los Acueductos Comunales, llevando a cabo" +
+    " que escapan a sus posibilidades como organizaciones locales. Asimismo, ha producido sinergias que la han convertido" +
+    " el interlocutor de los Acueductos que la conforman, lo que les ha permitido hablar con una sola voz ante entidades de" +
+    " internacional, la Municipalidad y el gobierno central, entre otros. UNAGUAS cuenta hoy con gran peso en términos" +
+    " incidencia política, de capacitación y en la promoción de una gestión integrada del recurso hídrico. Uno de sus objetivos" +
+    " es la consecución de recursos económicos para la adquisición de terrenos que fortalezcan la conservación y protección" +
+    " recurso hídrico. Con ello se contribuye en la conservación de cuencas hidrográficas y zonas de recarga acuífera de las que" +
+    " los manantiales captados por los distintos Acueductos Comunales afiliados. UNAGUAS no administra directamente los" +
+    ", sino que generar sinergias que permitan una gestión integrada del agua y de los territorios de donde se abastecen" +
+    " Acueductos afiliados. En esta línea de acción, UNAGUAS brindada un apoyo constante en el mejoramiento de las capacidades" +
+    " sus afiliados, procurando lograr una mejor calidad en el servicio a sus comunidades, además" +
+    " una mayor eficiencia en la prestación de ese servicio."
+    , {flex: 1}))
+
+    // nPageContent.push(this._createContentPageItem('Header',
+    // 'ENTIDAD, CREÍBLE Y SOSTENIBLE'
+    // , {flexWrap: 'wrap'}))
+    //
+    // nPageContent.push(this._createContentPageItem('Text',
+    // "UNAGUAS tiene hoy ya diez años de existencia y es mantenida integralmente, de manera voluntaria, por los" +
+    // " de los Acueductos Comunales que la conforman. Por otro lado, la organización ha ido fortaleciéndose" +
+    // " sí misma. Hace cuatro años dio el paso de tener una oficina y una persona para atender las necesidades" +
+    // " sus Acueductos miembros. Eso se mantiene hasta hoy, pero dentro de mejores oficinas y con una persona" +
+    // ". Además, en el último año estructuró su propio sitio WEB donde se brindan documentos," +
+    // ", eventos, proyectos y servicios, además desde 2009 comenzó a tener una participación asidua" +
+    // " diversos foros cantonales, nacionales e internacionales. De igual modo, su proceso de capacitación" +
+    // " ha incrementado, dándose en promedio uno o dos talleres de ese tipo por mes a los Acueductos" +
+    // ", como a los que no conforman la Unión. Asimismo, su reconocimiento y credibilidad se ha" +
+    // ", tanto dentro de los mismos Acueductos miembros como dentro del sector de Acueductos" +
+    // " del país, ante diversas instituciones y organizaciones y allende las fronteras. Hoy en" +
+    // ", tanto en el cantón como a nivel nacional, en temas de agua potable, recurso hídrico y otros," +
+    // " un actor indispensable. Todo lo anterior induce a pensar que la organización está en un franco" +
+    // " de desarrollo, de fortalecimiento y de consolidación, lo que implica sostenibilidad." +
+    // " necesidad de unirse. En el sector de los Acueductos Comunales costarricenses, su" +
+    // " pasará necesariamente por eso. La asociatividad es clave para el desarrollo" +
+    // " capacidades e iniciativas en común, todo lo cual lleva a fortalecimiento, consolidación," +
+    // " e incidencia. Sin cacao no hay chocolate. UNAGUAS ha demostrado que cuando" +
+    // " quiere se puede, pero que hay que mente aportan los Acueductos miembros son el compromiso" +
+    // " de cada uno de ellos con la experiencia, estar dispuestos a aportar. Los montos que" +
+    // " brindan los Acueductos son los que hacen vivir a UNAGUAS, por tanto, eso" +
+    // " con los mismos recursos del sector, puestos en común, aunque escasos, se pueden" +
+    // " grandes cosas."
+    // , {flexWrap: 'wrap'}))
+
+
+
 
     this.Content = nPageContent
   }
@@ -40,10 +112,35 @@ export default class AboutScreen extends React.Component {
 
     if (itemContent.type === 'Text') {
       return (
-        <Text>{itemContent.val}</Text>
+        <Text style={itemContent.style}>{itemContent.val}</Text>
+      )
+    }else if (itemContent.type === 'ScrollView') {
+      return (
+        <ScrollView>
+          <Text numberOfLines={4} style={itemContent.style}>{itemContent.val}</Text>
+        </ScrollView>
+      )
+    }else if (itemContent.type === 'Header') {
+      return (
+        <H1 style={itemContent.style}>{itemContent.val}</H1>
+      )
+    }else if (itemContent.type === 'List') {
+      return (
+        <List dataArray={itemContent.val}
+          renderRow={(item) =>
+            <ListItem style={itemContent.style}>
+              <Text style={itemContent.style}>{'\u2022'}  {item}</Text>
+            </ListItem>
+          }>
+        </List>
       )
     }
   }
+
+  _createContentPageItem(type, content, style = {}){
+    return {type: type, val: content, style: style}
+  }
+
 
   render(){
     return (
@@ -52,9 +149,11 @@ export default class AboutScreen extends React.Component {
           <Header title="UNAGUAS" back="false" menu="true" onMenuPress={() => {this.props.navigation.navigate('DrawerOpen')}}/>
         </View>
         <ScrollView>
-          <Content>
+          <Content style={{
+            padding: 10
+          }}>
             {this.Content && this.Content.map((item, index) => {
-              return this._renderDataContent(item, index)
+              return (<View style={{flex: 1}}>{this._renderDataContent(item, index)}</View>)
             }) }
           </Content>
         </ScrollView>
