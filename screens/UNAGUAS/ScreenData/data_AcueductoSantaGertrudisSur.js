065264161e87ba8f59032916c759e6e53e747d49
ealpizars <ealpizars@gmail.com>
2017-09-17 11:59:59 -0600

    Agregar AcueductoSantaGertrudisSur.

---
+++ screens/UNAGUAS/ScreenData/data_AcueductoSantaGertrudisSur.js
@@ -0,0 +1,10 @@
+export default [
+  {
+    type: "H2",
+    val: 'Acueducto Comunal Santa Gertrudis Sur'
+  },
+  {
+    type: 'Text',
+    val: 'Un acueducto que vela por brindar un servicio de cantidad y calidad, además de proteger sus fuentes.'
+  }
+]
