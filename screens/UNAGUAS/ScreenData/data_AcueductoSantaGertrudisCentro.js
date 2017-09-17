83ba54b7e595415d603e2268bbb098b856234601
ealpizars <ealpizars@gmail.com>
2017-09-17 12:00:39 -0600

    Cambio de nombre en data con el prefijo 'Acueducto' a SantaGertrudisCentro.

---
+++ screens/UNAGUAS/ScreenData/data_AcueductoSantaGertrudisCentro.js
@@ -0,0 +1,10 @@
+export default [
+{
+  type: "H2",
+  val: 'Santa Gertrudis Centro'
+},
+{
+  type: 'Text',
+  val: 'Somos una ASADA que lucha por preservar el agua y el medio ambiente, y brindar un servicio de calidad y continuidad a nuestros abonados.'
+}
+]
