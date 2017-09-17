5c95bb823116c9f6c8ef559519b86ad460c1b387
ealpizars <ealpizars@gmail.com>
2017-09-17 11:41:40 -0600

    Se agrega data item en lista de acueductos para Barrio latino.

---
+++ screens/UNAGUAS/ScreenData/data_AcueductoComunalBarrioLatino.js
@@ -0,0 +1,10 @@
+export default [
+{
+  type: "H2",
+  val: 'Barrio Latino'
+},
+{
+  type: 'Text',
+  val: 'Entidad de organización comunal, legalmente inscrita y delegada por A y A para brindar el servicio de agua potable, en cantidad, calidad y continuidad a la comunidad de Barrio Latino.'
+}
+]
