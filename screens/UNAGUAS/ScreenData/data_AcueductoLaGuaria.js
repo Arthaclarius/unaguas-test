b9a9e44797bcc067039d7de2e091173b850e8c19
ealpizars <ealpizars@gmail.com>
2017-09-17 12:01:57 -0600

    Cambio de nombre en data con el prefijo 'Acueducto' a LaGuaria.

---
+++ screens/UNAGUAS/ScreenData/data_AcueductoLaGuaria.js
@@ -0,0 +1,10 @@
+export default [
+  {
+    type: "H2",
+    val: 'Comunal La Guaria'
+  },
+  {
+    type: 'Text',
+    val: 'Asada responsable que vela por la salud de nuestra población en cuanto a la calidad del agua se refiere, cumpliendo con los estándares de calidad requeridos por el AyA.'
+  }
+]
