ac46d440bd8a2c5886366b53f715c2a73a62c889
ealpizars <ealpizars@gmail.com>
2017-10-07 01:26:27 -0600

    Cambios de prueba

--- screens/UNAGUAS/ScreenData/data_GalleryScreen.js
+++ screens/UNAGUAS/ScreenData/data_GalleryScreen.js
@@ -1,3 +1,4 @@
+<<<<<<< current
 import styles from '../../../assets/styles/UnaguasStyle'
 
 export default [
@@ -782,3 +783,229 @@ export default [
     ]
   }
 ]
+=======
+import styles from '../../../assets/styles/UnaguasStyle'
+
+export default [
+  {
+    type: 'Card',
+    val: [
+      {
+        type: 'View',
+        val: [
+          {
+            type: 'Image',
+            val: 'gallery-1',
+            style: styles.imageGallery
+          },
+          {
+            type: 'Text',
+            val: 'Vista',
+            style: styles.textGallery
+          }
+        ]
+      }
+    ]
+  },
+  {
+    type: 'Card',
+    val: [
+      {
+        type: 'View',
+        val: [
+          {
+            type: 'Image',
+            val: 'gallery-2',
+            style: styles.imageGallery
+          },
+          {
+            type: 'Text',
+            val: 'Reunion',
+            style: styles.textGallery
+          }
+        ]
+      }
+    ]
+  },
+  {
+    type: 'Card',
+    val: [
+      {
+        type: 'View',
+        val: [
+          {
+            type: 'Image',
+            val: 'gallery-3',
+            style: styles.imageGallery
+          },
+          {
+            type: 'Text',
+            val: 'Reunion',
+            style: styles.textGallery
+          }
+        ]
+      }
+    ]
+  },
+  {
+    type: 'Card',
+    val: [
+      {
+        type: 'View',
+        val: [
+          {
+            type: 'Image',
+            val: 'gallery-4',
+            style: styles.imageGallery
+          },
+          {
+            type: 'Text',
+            val: 'Reunion',
+            style: styles.textGallery
+          }
+        ]
+      }
+    ]
+  },
+  {
+    type: 'Card',
+    val: [
+      {
+        type: 'View',
+        val: [
+          {
+            type: 'Image',
+            val: 'gallery-5',
+            style: styles.imageGallery
+          },
+          {
+            type: 'Text',
+            val: 'Reunion',
+            style: styles.textGallery
+          }
+        ]
+      }
+    ]
+  },
+  {
+    type: 'Card',
+    val: [
+      {
+        type: 'View',
+        val: [
+          {
+            type: 'Image',
+            val: 'gallery-6',
+            style: styles.imageGallery
+          },
+          {
+            type: 'Text',
+            val: 'Reunion',
+            style: styles.textGallery
+          }
+        ]
+      }
+    ]
+  },
+  {
+    type: 'Card',
+    val: [
+      {
+        type: 'View',
+        val: [
+          {
+            type: 'Image',
+            val: 'gallery-7',
+            style: styles.imageGallery
+          },
+          {
+            type: 'Text',
+            val: 'Reunion',
+            style: styles.textGallery
+          }
+        ]
+      }
+    ]
+  },
+  {
+    type: 'Card',
+    val: [
+      {
+        type: 'View',
+        val: [
+          {
+            type: 'Image',
+            val: 'gallery-8',
+            style: styles.imageGallery
+          },
+          {
+            type: 'Text',
+            val: 'Reunion',
+            style: styles.textGallery
+          }
+        ]
+      }
+    ]
+  },
+  {
+    type: 'Card',
+    val: [
+      {
+        type: 'View',
+        val: [
+          {
+            type: 'Image',
+            val: 'gallery-9',
+            style: styles.imageGallery
+          },
+          {
+            type: 'Text',
+            val: 'Reunion',
+            style: styles.textGallery
+          }
+        ]
+      }
+    ]
+  },
+  {
+    type: 'Card',
+    val: [
+      {
+        type: 'View',
+        val: [
+          {
+            type: 'Image',
+            val: 'gallery-10',
+            style: styles.imageGallery
+          },
+          {
+            type: 'Text',
+            val: 'Reunion',
+            style: styles.textGallery
+          }
+        ]
+      }
+    ]
+  },
+  {
+    type: 'Card',
+    val: [
+      {
+        type: 'View',
+        val: [
+          {
+            type: 'Image',
+            val: 'gallery-11',
+            style: styles.imageGallery
+          },
+          {
+            type: 'Text',
+            val: 'ASADA SAN MIGUEL',
+            style: styles.textGallery
+          }
+        ]
+      }
+    ]
+  }
+]
+>>>>>>> before discard
